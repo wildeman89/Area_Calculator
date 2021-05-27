7c3949496ef8b54a0ede128bafb2b933bb6d8a36
gwilde89 <gwilde89@penguin>
2021-05-26 21:54:41 -0500

    I have added structs in this iteration. The next thing to do is to decide if I should implement a enum type called Shape/s and set all the structs into the enum. Looking at what Rust is trying to achieve, it does seem like setting these into an enum would be what I'm supposed to do as this will keep all related types correlated to each other.

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Writing a simple program to calculate the area of shapes. Mainly using to learn 
 # Part One
 This first part will be simply getting up and running. Getting user input, converting the input
 into floats in order to do calculations and everything else.
+
+# Part Two
+In this part, I will be adding structures into the program. Each shape type will be a structure.
+This should simplify the program.
--- area_calculator/src/main.rs
+++ area_calculator/src/main.rs
@@ -1,3 +1,38 @@
+// Going to place structs at the top
+struct Rectangle {
+    length: f64,
+    width: f64,
+}
+
+impl Rectangle {
+    fn area(&self) -> f64 {
+        self.length * self.width
+    }
+}
+
+struct Triangle {
+    base: f64,
+    height: f64,
+}
+
+impl Triangle {
+    fn area(&self) -> f64 {
+        (self.base * self.height) / 2.0
+    }
+}
+
+struct Circle {
+    radius: f64,
+}
+
+impl Circle {
+    fn area(&self) -> f64 {
+        let pi: f64 = 22.0 / 7.0;
+        pi * f64::powf(self.radius, 2.0)
+    }
+}
+
+
 fn main() {
     let mut run: bool = true;
     while run {
@@ -35,7 +70,7 @@ fn intro() {
     println!("\n\t\tWelcome to the Area Calculator");
     println!("\nThis program will calculate the area of a shape that you choose.");
     println!("Please choose one of the following shapes:");
-    println!("\t1)Rectangle/Square\n\t2)Triangle\n\t3)Circle\n\t4)QUIT");
+    println!("\t1) Rectangle/Square\n\t2) Triangle\n\t3) Circle\n\t4) QUIT");
 }
 
 fn convert_to_float(value: &String) -> f64 {
@@ -58,8 +93,8 @@ fn rectangle() {
     println!("Enter the following dimensions in order to get the area of your shape.");
     let length = input("Enter the length of the Rectangle/Square: ");
     let width = input("Enter the width of the Rectangle/Square: ");
-    let area = length * width;
-    println!("Length: {:.2}\nWidth: {:.2}\nArea: {:.2}", length, width, area);
+    let rect = create_rectangle(length, width);
+    println!("Length: {:.2}\nWidth: {:.2}\nArea: {:.2}", rect.length, rect.width, rect.area());
 }
 
 fn triangle() {
@@ -67,15 +102,38 @@ fn triangle() {
     println!("Enter the following dimensions in order to get the area of the triangle.");
     let base = input("Enter the base of the triangle: ");
     let height = input("Enter the height of the triangle: ");
-    let area = (base * height) / 2.0;
-    println!("Base: {:.2}\nHeight: {:.2}\nArea: {:.2}", base, height, area);
+    let tri = create_triangle(base, height);
+
+    println!("Base: {:.2}\nHeight: {:.2}\nArea: {:.2}", tri.base, tri.height, tri.area());
 }
 
 fn circle() {
     println!("\n\tCircle Area Calculator");
     println!("Enter the following dimension to get the area of your circle.");
     let radius = input("Enter the Radius of the circle: ");
-    let pi: f64 = 22.0 / 7.0;
-    let area = pi * f64::powf(radius, 2.0);
-    println!("Radius: {:.4}\nPI: {:.4}\nArea: {:.4}", radius, pi, area);
+    let circ = create_circle(radius);
+    println!("Radius: {:.4}\nArea: {:.4}", circ.radius, circ.area());
+}
+
+fn create_rectangle(length: f64, width: f64) -> Rectangle {
+    let rect = Rectangle {
+        length,
+        width,
+    };
+    rect
+}
+
+fn create_triangle(base: f64, height: f64) -> Triangle {
+    let tri = Triangle {
+        base,
+        height,
+    };
+    tri
+}
+
+fn create_circle(radius: f64) -> Circle {
+    let circ = Circle {
+        radius,
+    };
+    circ
 }
